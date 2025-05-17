678071d1e8c18a039d2a892744389878cfdf469d
mehemmed0 <memmedlimehemmed06@gmail.com>
2025-05-17 12:48:12 +0200

    Commit 1 of 800

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ ip = int(input("How many times do you want to commit? \n"))
 autoPush = input("Auto git push when committed? (y/n) \n")
 
 for i in range(ip):
-    os.system('git add .')
-    os.system(f'git commit -m "Commit {i + 1} of {ip}"')
+    os.system('git add .')  # This still works, but even without changes it will not block the commit
+    os.system(f'git commit --allow-empty -m "Commit {i + 1} of {ip}"')
 
 print(f"Committed {ip} times.")
 
